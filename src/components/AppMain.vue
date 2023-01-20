a7dcbb37a5e872cdfe1f5ecbbbbd68f604677bc2
Alessandro-Villani <alessvill@libero.it>
2023-01-20 17:08:23 +0100

    added main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+    <main>
+        <div class="content container">
+            <p> --&gt Content goes here &lt-- </p>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partial/mixins.scss' as *;
+@use '../assets/styles/partial/variables.scss' as *;
+
+main {
+    background-color: $black;
+    min-height: 120px;
+
+
+    p {
+        color: white;
+        font-size: 30px;
+        line-height: 120px;
+    }
+}
+</style>
